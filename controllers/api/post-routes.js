8c40247a2b74f1cb7e9ae647c37b6ca1745ce404
Seth Uschuk <suschuk24@gmail.com>
2020-10-31 08:36:38 -0500

    create upvote and comment functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -84,7 +84,25 @@ router.post('/', (req, res) => {
   });
   
 
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  console.log("PUT UPVOTE")
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    let param = { ...req.body, user_id: req.session.user_id }
+    console.log("PUT PARAM", param)
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
+});
+
 router.put('/:id', (req, res) => {
+  console.log("PUT ID")
     Post.update(
       {
         title: req.body.title
@@ -108,16 +126,7 @@ router.put('/:id', (req, res) => {
       });
   });
 
-// PUT /api/posts/upvote
-router.put('/upvote', (req, res) => {
-    // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-      .then(updatedPostData => res.json(updatedPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  });
+
 
 router.delete('/:id', (req, res) => {
   Post.destroy({
---
+++ public/javascript/upvote.js
@@ -0,0 +1,28 @@
+async function upvoteClickHandler(event) {
+    event.preventDefault();
+  
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+
+    console.log(id)
+  
+    const response = await fetch('/api/posts/upvote', {
+      method: 'PUT',
+      body: JSON.stringify({
+        post_id: id
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+console.log(response)
+    if(response.ok) {
+      // console.log('is something happening')
+      // document.location.reload();
+    } else {
+      alert(response.statusText)
+    }
+  };
+  
+  document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
