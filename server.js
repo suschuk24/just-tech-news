6784356180f12f3fc61a3e69e0e2f6229d5016df
Seth Uschuk <suschuk24@gmail.com>
2020-10-31 09:41:23 -0500

    optimized and formatted dates, added partials

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
@@ -14,6 +15,7 @@ const sess = {
     db: sequelize
   })
 };
+
 
 const path = require('path');
 
